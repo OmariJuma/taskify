2ee578364f9da03c6bf97c3b0d6e94e66624b60d
OmariJuma <jumao2669@gmail.comexi>
2024-06-11 00:45:38 +0300

    feat: I have added more functionality to the input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import { useState } from 'react'
 import './App.css'
 import InputField from './components/InputField'
+import { Todo } from './model';
 
 function App() {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
 
+  const handleAdd = (e:React.FormEvent)=>{
+    e.preventDefault();
+    if(todo){
+      setTodos([...todos, {
+        id:Date.now(),
+        todo, 
+        isDone: false
+      }]);
+      setTodo("");
+    }
+  }
+  console.log(todos)
   return (
     <div className="App">
       <h1 className='heading'>Taskify</h1>
-    <InputField/>
+    <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   )
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,8 +1,18 @@
 import "./styles.css";
- function InputField() {
+
+  interface Props{
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<String>>;
+    handleAdd: ()=>void;
+  };
+ function InputField({todo, setTodo, handleAdd}: Props ) {
   return (
-   <form className="input">
-    <input type="input" placeholder="Enter a task" className="input__box"/>
+   <form className="input" onSubmit={handleAdd}>
+    <input type="input"
+      placeholder="Enter a task" 
+      className="input__box"
+      onChange={(e)=>setTodo(e.target.value)}
+      />
     <button type="submit" className="input__submit">
         Go 
     </button>
