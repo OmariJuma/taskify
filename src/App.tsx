24017147b9f92068b566335457651c76fe0ef3e2
OmariJuma <jumao2669@gmail.comexi>
2024-06-11 00:09:43 +0300

    feat: I am creating a taskify project using typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import InputField from './components/InputField'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className='heading'>Taskify</h1>
+    <InputField/>
+    </div>
+  )
+}
+
+export default App
+ 
---
+++ src/components/InputField.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+ function InputField() {
+  return (
+   <form className="input">
+    <input type="input" placeholder="Enter a task" className="input__box"/>
+    <button type="submit" className="input__submit">
+        Go 
+    </button>
+   </form>
+  )
+}
+export default InputField;
