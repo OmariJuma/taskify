12d349bd618b5acfc4f44513a6fd335254428a5d
OmariJuma <jumao2669@gmail.comexi>
2024-06-12 00:53:22 +0300

    feat: I have added more functionality to this code

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import InputField from './components/InputField'
 import { Todo } from './model';
+import TodoList from './components/TodoList';
 
 function App() {
   const [todo, setTodo] = useState<string>("");
@@ -23,6 +24,7 @@ function App() {
     <div className="App">
       <h1 className='heading'>Taskify</h1>
     <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+   <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   )
 }
